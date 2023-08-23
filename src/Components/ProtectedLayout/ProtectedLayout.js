8d015b550b6235f48c92556d333b78bda400d463
vandanacr538 <vandanacr538@gmail.com>
2023-08-23 22:55:15 +0530

    From Your Watchlist implemented, when user not signed in-1(14Aug23)

--- src/Components/ProtectedLayout/ProtectedLayout.js
+++ src/Components/ProtectedLayout/ProtectedLayout.js
@@ -1,5 +1,8 @@
+import { AddBox } from "@mui/icons-material";
+import { Button } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
+import './protectedlayout.css';
 
 export default function ProtectedLayout() {
   const [isLoggedIn, setLoggedIn] = useState(true);
@@ -18,7 +21,13 @@ export default function ProtectedLayout() {
         </>
       ) : (
         <>
-          <h2 style={{marginLeft:"49px"}}>Sign in to access this</h2>
+          <h2 style={{marginLeft:"49px"}}>From Your Watchlist</h2>
+          <div className="unauth">
+            <AddBox style={{fontSize:"40px"}}/>
+            <p>Sign in to access your Watchlist</p>
+            <p>Save shows and movies to keep track of what you want to watch.</p>
+            <button className="signin-btn">Sign in to IMDb</button>
+          </div>
         </>
       )}
     </div>
