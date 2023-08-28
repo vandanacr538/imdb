0eaadb9d9eaa9a99deaea66c7c81286134dd720d
vandanacr538 <vandanacr538@gmail.com>
2023-08-28 23:27:16 +0530

    Added getMoviesTrailer func & updated url in Play component-1(17Aug23)

--- src/Components/Play/Play.js
+++ src/Components/Play/Play.js
@@ -1,25 +1,41 @@
 import { Box } from "@mui/material";
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 import ReactPlayer from "react-player";
 import { useParams } from "react-router-dom";
 
 export default function Play() {
   const [play, setPlay] = useState(false);
+  const [videoKey, setVideoKey] = useState();
   const {id}=useParams();
 
   const playVideo=()=>{
     setPlay(!play);
   }
+  const getMovieTrailers=async()=>{
+    const response=await axios.get("https://api.themoviedb.org/3/movie/" +id+ "/videos?language=en-US",
+    {
+      headers:{
+        Authorization:
+        "Bearer "+
+        "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNjhhYWU0YzYyNzFlNmNmZjUzODNlMGU5YjM3ZTRlYyIsInN1YiI6IjY0Y2U2YWY1NmQ0Yzk3MDBjYjdkYjg0YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0exlYdltt0_hYnHKl7FexczP3qg_sChBIeCZypZXsT0"
+      },
+    });
+    let movieTrailer=response.data.results.filter((element)=>{
+      return element.type==="Trailer" && element.name==="Official Trailer";
+    });
+    setVideoKey(movieTrailer[0].key);
+  }
 
   useEffect(()=>{
-    console.log("api call with "+id);
+    getMovieTrailers();
   }, []);
 
   return (
     <div>
       <Box sx={{ width: "100%" }}>
         <ReactPlayer
-          url="http://localhost:3000/movie.mp4"
+          url={"https://youtube.com/watch?v=" +videoKey}
           controls={true}
           height="550px"
           width="70%"
