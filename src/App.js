982a0a02b19303b412576b8ab130b1a01f0c9b72
vandanacr538 <vandanacr538@gmail.com>
2023-10-04 00:53:17 +0530

    Disabled BackToTopBtn when the page is not scrolled

--- src/App.js
+++ src/App.js
@@ -8,9 +8,11 @@ import Footer from './Components/Footer/Footer';
 import Login from './Pages/Login/Login';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { useEffect, useState } from 'react';
+import { KeyboardArrowUp } from '@mui/icons-material';
 
 function App() {
   const [authButton, setAuthButton]=useState("Sign In");
+  const [displayBTTBtn, setDisplayBTTBtn]=useState("");
   const scrollToTop=()=>{
     window.scroll({top:0, behavior:"smooth"});
   }
@@ -18,11 +20,22 @@ function App() {
     if(localStorage.getItem("token")){
       setAuthButton("Sign Out");
     }
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 1400) {
+        setDisplayBTTBtn("flex");
+      } 
+      else {
+        setDisplayBTTBtn("");
+      }
+    });
   }, []);
   return (
     <GoogleOAuthProvider clientId='484151294674-jno436mqufdpsenrg2csnllfdpu8f1g8.apps.googleusercontent.com'>
       <div>
-      <button className='back-to-top-btn' onClick={scrollToTop}>Back to top</button>
+      <button className='back-to-top-btn' onClick={scrollToTop} style={{display:displayBTTBtn}}>
+        <KeyboardArrowUp/>
+        Back to top
+      </button>
       <BrowserRouter>
       <Header authButton={authButton}/>
       <Ad/>
