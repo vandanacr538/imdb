e1ee5ed9fe00ca3d47007d0f453cedc8cd0abc31
vandanacr538 <vandanacr538@gmail.com>
2023-10-06 11:45:39 +0530

    LogIn form error validation

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,25 +1,67 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import './login.css';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { GoogleLogin } from '@react-oauth/google';
-import Cookies from 'js-cookie';
 import History from '../../Components/History/History';
 
 export default function Login(props) {
   const [loginData, setLoginData]=useState({});
+  const [loginError, setLoginError]=useState("");
+  const [emptyError, setEmptyError]=useState({usernameError:"", passwordError:""});
   const navigate=useNavigate();
 
   const handleChangeLoginData=(e)=>{
-    setLoginData((previousData)=>{
-      setLoginData({...previousData, [e.target.name]:e.target.value})
-    });
+    setLoginData((previousData)=>({...previousData, [e.target.name]:e.target.value}));
   }
   const validataLogin=async()=>{
-    const response=await axios.post("http://localhost:8080/login/loginapi", loginData);
-    localStorage.setItem("token", response.data.token);
-    props.setAuthButton("Sign Out");
-    navigate("/");
+    setLoginError("");
+    setEmptyError((previousValue)=>({...previousValue, 
+      usernameError:"",
+      passwordError:""
+    }));
+    if(Object.keys(loginData).length<=1 || loginData.username==="" || loginData.password===""){
+      if(Object.keys(loginData).length===0){
+        setEmptyError((previousValue)=>({...previousValue, 
+          usernameError:"",
+          passwordError:""
+        }));
+      }
+      if(loginData.username==="" || !("username" in loginData)){
+        setEmptyError((previousValue)=>({...previousValue, usernameError:"Enter your username or email"}));
+      }
+      else{
+        setEmptyError((previousValue)=>({...previousValue, usernameError:""}));
+      }
+      if(loginData.password==="" || !("password" in loginData)){
+        setEmptyError((previousValue)=>({...previousValue, passwordError:"Enter your password"}));
+      }
+      else{
+        setEmptyError((previousValue)=>({...previousValue, passwordError:""}));
+      }
+    } 
+    else{
+      try{
+        let result=await axios.post("http://localhost:8080/login/loginapi", loginData);
+        if(result.status===200){
+          localStorage.setItem("token", result.data.token);
+          props.setAuthButton("Sign Out");
+          navigate("/");
+        }
+      }
+      catch(e){
+        if(e.response.status===403){
+          console.log("403");
+          setLoginError(e.response.data.msg);
+        }
+        else if(e.response.status===401){
+          setLoginError(e.response.data.msg);
+        }
+        else{
+          setLoginError(e.response.data.msg);
+        }
+      }
+    }
   }
   const sendToken=async(token)=>{
     const response=await axios.post("http://localhost:8080/login/oauth", {token:token.credential});
@@ -36,10 +78,19 @@ export default function Login(props) {
           <div className='signin-fields'>
               <label for="email">Email</label>
               <input type='text' id='email' className='signin-input' name="username" onChange={handleChangeLoginData}></input>
+              <p className={emptyError.usernameError!="" ? "empty-login-error" : "no-login-error"}>
+                {emptyError.usernameError}
+              </p>
           </div>
           <div className='signin-fields'>
               <label for="password">Password</label>
               <input type='text' id='password' className='signin-input' name="password" onChange={handleChangeLoginData}></input>
+              <p className={emptyError.passwordError!="" ? "empty-login-error" : "no-login-error"}>
+                {emptyError.passwordError}
+              </p>
+              <p className={loginError!="" ? "login-error" : "no-login-error" }>
+                {loginError}
+              </p>
           </div>
           <div className='signin-fields'>
               <button className='signin' onClick={validataLogin}>Sign in</button>
