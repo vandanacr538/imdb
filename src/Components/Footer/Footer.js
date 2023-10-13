0a7896b176ad467495307ce5a0b6b68d6993c024
vandanacr538 <vandanacr538@gmail.com>
2023-10-14 02:49:33 +0530

    Added functionality to footer icons & removed footer menu

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,32 +1,19 @@
 import React from 'react'
 import './footer.css';
 import { Facebook, Instagram, Telegram, Twitter, YouTube } from '@mui/icons-material';
+import { useNavigate } from 'react-router-dom';
 
 export default function Footer() {
+  const navigate=useNavigate();
   return (
     <>
         <footer>
-            <button className='footer-signin-btn'>Sign in for more access</button>
+            <button className='footer-signin-btn' onClick={()=>navigate("/login")}>Sign in for more access</button>
             <div className='footer-smlinks'>
-                <a href='' className='footer-sm-link'><Instagram /></a>
-                <a href='' className='footer-sm-link'><Twitter /></a>
-                <a href='' className='footer-sm-link'><YouTube /></a>
-                <a href='' className='footer-sm-link'><Facebook /></a>
-                <a href='' className='footer-sm-link'><Telegram /></a>
-            </div>
-            <div className='footer-menu'>
-                <a href=''>Get the IMDb App</a>
-                <a href=''>Help</a>
-                <a href=''>Site Index</a>
-                <a href=''>IMDb Pro</a>
-                <a href=''>Box Office Mojo</a>
-                <a href=''>IMDb Developer</a>
-                <a href=''>Press Room</a>
-                <a href=''>Advertising</a>
-                <a href=''>Jobs</a>
-                <a href=''>Condition of Use</a>
-                <a href=''>Privacy Policy</a>
-                <a href=''>Your Ads Privacy Choices</a>
+                <a href='https://www.instagram.com/imdb/' target='blank' className='footer-sm-link'><Instagram /></a>
+                <a href='https://twitter.com/imdb' target='blank' className='footer-sm-link'><Twitter /></a>
+                <a href='https://www.youtube.com/imdb' target='blank' className='footer-sm-link' ><YouTube /></a>
+                <a href='https://www.facebook.com/imdb' target='blank' className='footer-sm-link'><Facebook /></a>
             </div>
             <p>an amazon company</p>
             <p className='footer-copyright'>© 1990-2023 by IMDb.com, Inc.</p>
