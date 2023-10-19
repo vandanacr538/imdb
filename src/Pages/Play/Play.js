017f9f68b40619957bab0a32f034c0238825b2a8
vandanacr538 <vandanacr538@gmail.com>
2023-10-20 01:40:00 +0530

    Updated Play Page UI and functionality

--- src/Pages/Play/Play.js
+++ src/Pages/Play/Play.js
@@ -1,56 +1,121 @@
-import { Box } from "@mui/material";
+import {
+  Close,
+  ErrorOutline,
+  PauseOutlined,
+  PlayArrow,
+  ReplayOutlined,
+} from "@mui/icons-material";
+import { Box, CircularProgress } from "@mui/material";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import ReactPlayer from "react-player";
-import { useParams } from "react-router-dom";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
+import "./play.css";
 
 export default function Play() {
-  const [play, setPlay] = useState(false);
+  const [play, setPlay] = useState(true);
+  const [videoEnd, setVideoEnd] = useState(false);
   const [videoKey, setVideoKey] = useState();
-  const {id}=useParams();
+  const navigate = useNavigate();
+  const { id } = useParams();
 
-  const playVideo=()=>{
+  const playVideo = () => {
     setPlay(!play);
-  }
-  const getMovieTrailers=async()=>{
-    const response=await axios.get("https://api.themoviedb.org/3/movie/" +id+ "/videos?language=en-US",
-    {
-      headers:{
-        Authorization:
-        "Bearer "+
-        "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNjhhYWU0YzYyNzFlNmNmZjUzODNlMGU5YjM3ZTRlYyIsInN1YiI6IjY0Y2U2YWY1NmQ0Yzk3MDBjYjdkYjg0YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0exlYdltt0_hYnHKl7FexczP3qg_sChBIeCZypZXsT0"
-      },
+    setVideoEnd(false);
+  };
+  const handleVideoStart = () => {
+    setPlay(true);
+    setVideoEnd(false);
+  };
+  const handleVideoEnded = () => {
+    setPlay(false);
+    setVideoEnd(true);
+  };
+  const getMovieTrailers = async () => {
+    const response = await axios.get(
+      "https://api.themoviedb.org/3/movie/" + id + "/videos?language=en-US",
+      {
+        headers: {
+          Authorization:
+            "Bearer " +
+            "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNjhhYWU0YzYyNzFlNmNmZjUzODNlMGU5YjM3ZTRlYyIsInN1YiI6IjY0Y2U2YWY1NmQ0Yzk3MDBjYjdkYjg0YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0exlYdltt0_hYnHKl7FexczP3qg_sChBIeCZypZXsT0",
+        },
+      }
+    );
+    let movieTrailer = response.data.results.filter((element) => {
+      return (
+        element.type === "Trailer" &&
+        (element.name === "Official Trailer" ||
+          element.name === "Official US Trailer")
+      );
     });
-    let movieTrailer=response.data.results.filter((element)=>{
-      return element.type==="Trailer" && element.name==="Official Trailer";
-    });
-    setVideoKey(movieTrailer[0].key);
-  }
+    if (movieTrailer.length > 0) {
+      setVideoKey(movieTrailer[0].key);
+    } else {
+      setVideoKey("Not available");
+    }
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     getMovieTrailers();
   }, []);
 
   return (
-    <div>
+    <div className="play-container">
       <Box sx={{ width: "100%" }}>
-        <ReactPlayer
-          url={"https://youtube.com/watch?v=" +videoKey}
-          controls={true}
-          height="550px"
-          width="70%"
-          playing={play}
-          onStart={()=>{
-            setPlay(true);
-            console.log("onstart after: "+play);
-          }}
-          onPause={()=>{
-            console.log("onpause before: "+play);
-            setPlay(false);
-            console.log("onspause after: "+play);
-          }}
-        />
-        <button onClick={playVideo}>{play ? "Pause" : "Play"}</button>
+        {videoKey ? (
+          <>
+            <button
+              className="play-page-close-icon"
+              onClick={() => navigate(-1)}
+            >
+              <Close />
+              <span>Close</span>
+            </button>
+            <ReactPlayer
+              url={"https://youtube.com/watch?v=" + videoKey}
+              controls={true}
+              height="550px"
+              width="100%"
+              playing={play}
+              onStart={handleVideoStart}
+              onReady={() => setPlay(true)}
+              onPlay={() => setPlay(true)}
+              onPause={() => setPlay(false)}
+              onEnded={handleVideoEnded}
+            />
+            {videoKey === "Not available" ? (
+              <div className="video-not-available">
+                <ErrorOutline style={{ fontSize: "100px", color: "#bebebe" }} />
+                <span>Video Unavailable now</span>
+              </div>
+            ) : (
+              <>
+                <div className="play-pause-btn" onClick={playVideo}>
+                  {videoEnd ? (
+                    <>
+                      <ReplayOutlined style={{ fontSize: "80px" }} />
+                    </>
+                  ) : (
+                    <>
+                      {play ? (
+                        <PauseOutlined style={{ fontSize: "80px" }} />
+                      ) : (
+                        <PlayArrow style={{ fontSize: "80px" }} />
+                      )}
+                    </>
+                  )}
+                </div>
+              </>
+            )}
+          </>
+        ) : (
+          <div className="play-page-loader">
+            <CircularProgress
+              style={{ color: "#fff", width: "80px", height: "80px" }}
+            />
+          </div>
+        )}
       </Box>
     </div>
   );
